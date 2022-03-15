86facf07eba7507ad115a4a12b09ecf2318ebe89
Arun Appasaheb Billur <arun.billur@valuelabs.com>
2022-03-15 18:21:20 +0530

    Adding 1st assginment work

---
+++ assignment_1/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import { Employee } from "./employee";
+function App() {
+  return (
+    <div className="App">
+      <Employee></Employee>
+    </div>
+  );
+}
+
+export default App;
